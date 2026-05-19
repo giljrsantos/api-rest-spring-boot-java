375fd307da8dd7149c13ea15d7e4cf33ed441fed
Gilberto Junior <giljrsantos@gmail.com>
2026-05-19 14:49:48 -0300

    Add methods for number conversion and validation in NumberConverter class

--- src/main/java/br/com/br/firstapispringboot/request/convertes/NumberConverter.java
+++ src/main/java/br/com/br/firstapispringboot/request/convertes/NumberConverter.java
@@ -1,4 +1,20 @@
 package br.com.br.firstapispringboot.request.convertes;
 
+import br.com.br.firstapispringboot.exception.UnsupportedMathOperationException;
+
 public class NumberConverter {
+
+    public static Double convertToDouble(String strNumber) throws IllegalArgumentException {
+        if(strNumber == null || strNumber.isEmpty()) throw new UnsupportedMathOperationException("Please set a numeric value");
+        String number = strNumber.replace(",", "."); // R$ 5,00 USD 5.0
+        return Double.parseDouble((number));
+    }
+
+    public static boolean isNumeric(String strNumber) {
+        if(strNumber == null || strNumber.isEmpty()) return false;
+
+        String number = strNumber.replace(",", "."); // R$ 5,00 USD 5.0
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+
+    }
 }
