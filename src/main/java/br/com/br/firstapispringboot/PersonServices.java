b62463803bb53394cd79a446f554e72c206c7868
Gilberto Junior <giljrsantos@gmail.com>
2026-06-01 20:02:52 -0300

    Add findAll method to PersonController and PersonServices for retrieving all Person entities

--- src/main/java/br/com/br/firstapispringboot/PersonServices.java
+++ src/main/java/br/com/br/firstapispringboot/PersonServices.java
@@ -3,6 +3,8 @@ package br.com.br.firstapispringboot;
 import br.com.br.firstapispringboot.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -12,6 +14,17 @@ public class PersonServices {
     private  final AtomicLong counter = new AtomicLong();
 
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public List<Person> findAll(){
+        List<Person> persons = new ArrayList<Person>();
+
+        for(int i = 0; i < 8; i++){
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
 
     public Person findById(String id){
         logger.info("Finding one Person!");
@@ -26,4 +39,15 @@ public class PersonServices {
 
         return person;
     }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("FirstName " + i);
+        person.setLastName("LastName " + i);
+        person.setGender("Male");
+        person.setAddress("Some Address in Brazil" + i);
+        return person;
+    }
+
 }
--- src/main/java/br/com/br/firstapispringboot/controllers/PersonController.java
+++ src/main/java/br/com/br/firstapispringboot/controllers/PersonController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 import java.awt.*;
 import java.util.logging.Logger;
@@ -20,6 +21,14 @@ public class PersonController {
     @Autowired
     private PersonServices service;
 
+    @RequestMapping(
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public List<Person> findAll(){
+        return service.findAll();
+    }
+
     @RequestMapping(value = "/{id}",
     method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE
@@ -27,6 +36,8 @@ public class PersonController {
     public Person findById(@PathVariable("id") String id){
         return service.findById(id);
     }
+
+
 
     //private Logger logger = Logger.getLogger(PersonController.class.getName());
 
